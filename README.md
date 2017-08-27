5ad4acc1ee0647db3d9cf2253c119c0b7358e1a6
cc <lufengc@163.com>
2017-08-27 22:27:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# elasticsearch-boot
+
+基于Spring Boot + elasticsearch 搭建的全文搜索示例
+
+
---
+++ src/main/java/com/bdfint/es/ESApplication.java
@@ -0,0 +1,17 @@
+package com.bdfint.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fengcheng
+ * @version 2017/8/26
+ */
+@SpringBootApplication
+public class ESApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESApplication.class, args);
+    }
+
+}
