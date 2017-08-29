814b7b75f9c2709e0ceb06d989519596d7d61699
fengcheng <lufengcheng@bdfint.com>
2017-08-29 17:58:28 +0800

    init

--- src/main/java/com/bdfint/es/service/ArticleService.java
+++ src/main/java/com/bdfint/es/service/ArticleService.java
@@ -5,6 +5,8 @@ import com.bdfint.es.bean.Article;
 import com.bdfint.es.dao.ArticleRepository;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,8 @@ import java.util.List;
 @Service
 public class ArticleService {
 
+    private static final Logger logger = LoggerFactory.getLogger(ArticleService.class);
+
     private final ArticleRepository articleRepository;
 
     @Autowired
@@ -24,15 +28,47 @@ public class ArticleService {
 
     public void initData() {
         List<Article> articleList = new ArrayList<>();
-        articleList.add(new Article("1", "那天", "我还清晰地记得那天我们坐在江边聊天的情境", "关键词",0L, new Date()));
-        articleList.add(new Article("2", "那天", "我记得我们聊天的情境", "关键词",0L, new Date()));
-        articleList.add(new Article("2", "那天", "我们聊天的情境", "关键词",0L, new Date()));
+        articleList.add(new Article("1", "我们", "我还清晰地记得那天我们坐在江边聊天的情境", "关键词", 0L, new Date()));
+        articleList.add(new Article("2", "我们", "我记得我们聊天的情境", "关键词", 0L, new Date()));
+        articleList.add(new Article("2", "我们", "我们聊天的情境", "关键词", 0L, new Date()));
         articleRepository.saveAll(articleList);
     }
 
-    public String search(String param) {
-        QueryBuilder queryBuilde = QueryBuilders.multiMatchQuery(param, "name", "content");
-        Iterable<Article> search = articleRepository.search(queryBuilde);
-        return JSON.toJSONString(search);
+    public void save(Article article) {
+        articleRepository.save(article);
     }
+
+    public String search(Integer pageNo, Integer pageSize, String searchContent) {
+        QueryBuilder queryBuilde = QueryBuilders.multiMatchQuery(searchContent, "name", "content");
+
+
+        Iterable<Article> result = articleRepository.search(queryBuilde);
+
+//        // 分页参数
+//        Pageable pageable = new PageRequest(pageNo, pageSize);
+//
+//        // 短语匹配到的搜索词，求和模式累加权重分
+//        // 权重分查询 https://www.elastic.co/guide/cn/elasticsearch/guide/current/function-score-query.html
+//        //   - 短语匹配 https://www.elastic.co/guide/cn/elasticsearch/guide/current/phrase-matching.html
+//        //   - 字段对应权重分设置，可以优化成 enum
+//        //   - 由于无相关性的分值默认为 1 ，设置权重分最小值为 10
+//        // Function Score Query
+//
+//        QueryBuilder queryBuilde = QueryBuilders.multiMatchQuery(searchContent, "name", "content");
+//        FunctionScoreQueryBuilder functionScoreQueryBuilder = QueryBuilders.functionScoreQuery(queryBuilde);
+//        // 创建搜索 DSL 查询
+//        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+//                .withPageable(pageable)
+//                .withQuery(functionScoreQueryBuilder)
+//                .build();
+//
+//        logger.info("\n searchCity(): searchContent [" + searchContent + "] \n DSL  = \n " + searchQuery.getQuery().toString());
+//
+//        Page<Article> result = articleRepository.search(searchQuery);
+
+
+        return JSON.toJSONString(result);
+    }
+
+
 }
--- src/main/java/com/bdfint/es/web/ArticleController.java
+++ src/main/java/com/bdfint/es/web/ArticleController.java
@@ -1,5 +1,6 @@
 package com.bdfint.es.web;
 
+import com.bdfint.es.bean.Article;
 import com.bdfint.es.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +19,19 @@ public class ArticleController {
     }
 
     @RequestMapping(value = "/init", method = RequestMethod.GET)
-    public String init(String param) throws Exception {
+    public String init() throws Exception {
         articleService.initData();
         return "init success!";
     }
 
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Article article) throws Exception {
+        articleService.save(article);
+        return "success!";
+    }
+
     @RequestMapping(value = "/search", method = RequestMethod.GET)
-    public String search(String param) throws Exception {
-        return articleService.search(param);
+    public String search(Integer pageNo, Integer pageSize, String searchContent) throws Exception {
+        return articleService.search(pageNo, pageSize, searchContent);
     }
 }
