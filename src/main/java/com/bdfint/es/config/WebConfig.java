084f609818fd8ce480d4a9a54eada12328104b31
fengcheng <lufengcheng@bdfint.com>
2017-08-31 16:50:23 +0800

    init

---
+++ src/main/java/com/bdfint/es/config/WebConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017. <a href="http://www.lufengc.com">lufengc</a> All rights reserved.
+ */
+
+package com.bdfint.es.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fengcheng
+ * @version 2017/8/31
+ */
+@Configuration
+public class WebConfig {
+
+
+}
